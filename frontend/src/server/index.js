c8c5ac99efc1f98eac1b53e4fa7500491a8e9c99
Christopher Schmidt <fakod666@googlemail.com>
2017-11-23 10:06:43 +0100

    initial

---
+++ frontend/src/server/index.js
@@ -0,0 +1,51 @@
+const AssetsRoute = require('./routes/assets')
+const Hapi = require('hapi')
+const Hoek = require('hoek')
+const IndexRoute = require('./routes')
+const BooksRoute = require('./routes/books')
+const path = require('path')
+const server = new Hapi.Server()
+const uriPrefix =  process.env.URI_PREFIX || ''
+
+server.connection({
+  port: process.env.PORT || 80,
+  host: '0.0.0.0',
+  router: {
+    stripTrailingSlash: true
+  }
+})
+
+
+server.register([
+  require('vision'),
+  require('inert')
+], err => {
+  Hoek.assert(!err, err)
+
+  server.views({
+    engines: {
+      html: require('handlebars')
+    },
+    relativeTo: __dirname,
+    path: 'views',
+    layout: true,
+    layoutPath: path.join('views', 'layouts'),
+    partialsPath: path.join('views', 'partials'),
+    context: {
+      uriPrefix: uriPrefix
+    }
+  })
+
+  server.route(IndexRoute.get(uriPrefix || '/'))
+  server.route(BooksRoute.get(`${uriPrefix}/books`))
+  server.route(AssetsRoute.get(`${uriPrefix}/assets/{param*}`))
+})
+
+module.exports.start = function () {
+  server.start(err => {
+    if (err) {
+      throw err
+    }
+    console.log(`Server running at: ${server.info.uri}`)
+  })
+}
